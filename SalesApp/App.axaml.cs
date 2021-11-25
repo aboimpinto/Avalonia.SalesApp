f9c7c1a3684aca65dcbb351eadd53165326fdeb7
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-11-26 00:02:27 +0100

    SalesApp Avalonia.MVVM project

---
+++ SalesApp/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using SalesApp.ViewModels;
+using SalesApp.Views;
+
+namespace SalesApp
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
