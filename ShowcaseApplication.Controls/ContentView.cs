4687266a288dc011665dc7924f219d5ef05e25da
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-12-17 02:40:13 +0100

    add buttons to the ContentView and buttons property

--- ShowcaseApplication.Controls/ContentView.cs
+++ ShowcaseApplication.Controls/ContentView.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Styling;
 
 namespace ShowcaseApplication.Controls;
@@ -6,6 +7,15 @@ namespace ShowcaseApplication.Controls;
 public class ContentView : ContentControl, IStyleable
 {
     Type IStyleable.StyleKey => typeof(ContentControl);
+
+    public static readonly AvaloniaProperty ButtonsProperty = 
+        AvaloniaProperty.Register<ContentView, ContentControl>(nameof(Buttons));
+
+    public object Buttons
+    {
+        get => (ContentControl)GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
+    }
 
     public ContentView()
     {
