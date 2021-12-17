0d61003741f0933920b512a3b23404f517d36a8b
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-12-17 02:11:59 +0100

    ContentView shows the content

--- ShowcaseApplication.Controls/ContentView.cs
+++ ShowcaseApplication.Controls/ContentView.cs
@@ -1,8 +1,13 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Styling;
 
 namespace ShowcaseApplication.Controls;
 
-public class ContentView : ContentControl
+public class ContentView : ContentControl, IStyleable
 {
+    Type IStyleable.StyleKey => typeof(ContentControl);
 
+    public ContentView()
+    {
+    }
 }
